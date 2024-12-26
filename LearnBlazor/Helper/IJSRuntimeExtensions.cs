2d201db29d62e3137a6168b88b4c1053a0146e35
Elston Misquitta <elston007@gmail.com>
2024-12-26 21:16:22 +0530

    Toastr / Sweet Alert Notifications
    Toastr / Sweet Alert Notifications

---
+++ LearnBlazor/Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper;
+
+public static class IJSRuntimeExtensions
+{
+    public static async Task toastrSuccess(this IJSRuntime js, string message)
+    {
+        await js.InvokeVoidAsync("showToastr","success", message);
+    }
+
+    public static async Task toastrError(this IJSRuntime js, string message)
+    {
+        await js.InvokeVoidAsync("showToastr", "error", message);
+    }
+}
---
+++ LearnBlazor/wwwroot/js/common.js
@@ -0,0 +1,18 @@
+﻿// (1) Toastr Notification
+window.showToastr = function (type, message) {
+    if (type === "success") {
+        toastr.success(message, { timeOut: 1000 });
+    }
+    if (type === "error") {
+        toastr.error(message, { timeOut: 1000 });
+    }
+}
+
+// (2) Sweet Alert Notification
+window.sweetalert2 = function (type, message) {
+    Swal.fire({
+        title: "Sweet Alert Notfication",
+        text: message,
+        icon: type
+    });
+}
